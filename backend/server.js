26593963fd25da7207b551b4796e6dd483b13bd0
Adil <adilshahzad684@gmail.com>
2024-05-28 22:54:17 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://adil:adil24@cluster0.jej5m5q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+// Schema and Model
+const DataSchema = new mongoose.Schema({
+    label: String,
+    value: Number,
+});
+
+const Data = mongoose.model('Data', DataSchema);
+
+// API endpoint to get data
+app.get('/data', async (req, res) => {
+    try {
+        const data = await Data.find();
+        res.json(data);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
